0229bb1dfbeaa0817c087962b06f89a8a04a6694
Galen CC <galen8183@gmail.com>
2025-12-09 17:11:37 -0500

    session: move autostart from dwm/bashrc to xinitrc

--- .config/autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-while true; do
-	rescrobbled >> "$XDG_CACHE_HOME/rescrobbled.log" &
-	sleep 2h
-	pkill rescrobbled
-	printf "[%s] killed rescrobbled, restarting...\n\n" "$(date -Is)" >> "$XDG_CACHE_HOME/rescrobbled.log"
-done
